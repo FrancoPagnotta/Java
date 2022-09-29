99b1a12466fa8d7f9e4ff4c1ac6cfa4f65d9a6f5
unknown <francoapagnotta@gmail.com>
2022-09-29 18:45:57 -0300

    input of numeric values using the window input dialog

--- src/SistemasNumericos.java
+++ src/SistemasNumericos.java
@@ -1,19 +1,44 @@
+import javax.lang.model.SourceVersion;
+import javax.swing.*;
+
 public class SistemasNumericos {
     public static void main(String[] args) {
 
-        int decimalNumber = 500;
+        String numberStr = JOptionPane.showInputDialog("Enter an integer number");
+        int decimalNumber = 0;
+
+        try {
+            decimalNumber = Integer.parseInt(numberStr);
+        } catch (NumberFormatException error) {
+            JOptionPane.showMessageDialog(null, "Please, only numerics values");
+            main(args);
+            System.exit(0);
+        }
+
         System.out.println("decimalNumber = " + decimalNumber);
 
-        System.out.println("binary number of " + decimalNumber + " = " + Integer.toBinaryString(decimalNumber));
+        String binaryNumberResult = "binary number of " + decimalNumber + " = " + Integer.toBinaryString(decimalNumber);
+        System.out.println("binary number = " + binaryNumberResult);
+
         int binaryNumber = 0b111110100;
         System.out.println("binaryNumber displayed as  decimal = " + binaryNumber);
 
-        System.out.println("octal number of " + decimalNumber + " = " + Integer.toOctalString(binaryNumber));
+        String octalNumberResult = "octal number of " + decimalNumber + " = " + Integer.toOctalString(binaryNumber);
+        System.out.println("octal number = " + octalNumberResult);
+
         int octalNumber = 0764;
         System.out.println("octalNumber displayed as decimal = " + octalNumber);
 
-        System.out.println("hexadecimal number of " + decimalNumber + " = " + Integer.toHexString(decimalNumber));
+        String hexadecimalNumberResult = "hexadecimal number of " + decimalNumber + " = " + Integer.toHexString(decimalNumber);
+        System.out.println("hexadecimal number = " + hexadecimalNumberResult);
+
         int hexNumber = 0x1f4;
         System.out.println("hexNumber displayed as decimal number = " + hexNumber);
+
+        String message = binaryNumberResult;
+        message += "\n" + octalNumberResult;
+        message += "\n" + hexadecimalNumberResult;
+
+        JOptionPane.showMessageDialog(null, message);
     }
 }
