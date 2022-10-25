9486bfae8dcf045e5412f62cb888962803ee8be7
unknown <francoapagnotta@gmail.com>
2022-10-25 01:50:12 -0300

    exercise with ternary operators

---
+++ src/OperadorTernarioNumeroMayor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OperadorTernarioNumeroMayor {
+    public static void main(String[] args) {
+
+        int max = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Ingrese un segundo numero");
+        int number2 = scanner.nextInt();
+
+        System.out.println("Ingrese un tercer numero");
+        int number3 = scanner.nextInt();
+
+        max = (number1 > number2 ) ? number1 : (number2);
+        max = (max > number3) ? max : number3;
+
+        System.out.println("max = " + max);
+                
+                
+    }
+}
