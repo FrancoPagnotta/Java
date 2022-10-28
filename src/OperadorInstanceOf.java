45d92902cbae8864ac67348f0ccc305c77a3a57f
unknown <francoapagnotta@gmail.com>
2022-10-28 01:12:53 -0300

    instance of operators

--- src/OperadorInstanceOf.java
+++ src/OperadorInstanceOf.java
@@ -5,8 +5,26 @@ public class OperadorInstanceOf {
         Integer num = 5;
         // both are objects, not primitives values, because String and Integer are wrapper class, so behind the scenes is occurring the instances like new String() and new Integer()
 
-        boolean b1 = text instanceof String;
-        System.out.println("text es del tipo String = " + b1);
+        Boolean b1 = text instanceof String;
+        System.out.println("text es de tipo String = " + b1);
 
+        b1 = text instanceof Object;
+        System.out.println("text es de tipo Object = " + b1);
+
+        b1 = num instanceof Integer;
+        System.out.println("num es de tipo Integer = " + b1);
+
+        b1 = num instanceof Number;
+        System.out.println("num es de tipo Number = " + b1);
+
+        b1 = num instanceof Object;
+        System.out.println("num es de tipo Object = " + b1);
+
+        Double decimal = 10.50;
+        b1 = decimal instanceof Number;
+        System.out.println("decimal es de tipo NUmber = " + b1);
+
+        b1 = b1 instanceof Boolean;
+        System.out.println("b1 es de tipo Boolean = " + b1);
     }
 }
